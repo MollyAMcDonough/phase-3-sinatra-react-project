b3567a7e2fa43841318174d3f7ad5445bec3322c
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-29 19:44:01 -0400

    added post, patch, and delete endpoints to start with. need to review data from endpoint further to finishe them and test

--- app/controllers/task_times_controller.rb
+++ app/controllers/task_times_controller.rb
@@ -5,4 +5,28 @@ class TaskTimesController < ApplicationController
     end
  
     # Post, Patch, Delete endpoints
+    delete '/task_times/:id' do
+        TaskTime.find(params[:id]).destroy.to_json
+    end
+
+    patch '/task_times/:id' do
+        task_time = TaskTime.find(params[:id])
+        task_time.update(params)
+        task_time.to_json
+    end
+
+    post '/task_times' do
+        columns = TaskDef.column_names
+        attributes = {}
+        columns.each do |name|
+            sym = name.to_sym
+            if params.keys.include?(sym)
+                attributes[sym] = params[sym]
+            end
+        end
+        task_def_attr
+        td = TaskDef.create(attributes)
+        TaskTime.create(task_def: td, start: params[:startDate], end: params[:endDate]).to_json
+    end
+
 end
